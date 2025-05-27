00e4d3c0c2f6d03b05595626892a7ba2da9ac178
road44 <h0ld4@h0ld4.eu>
2025-05-27 10:57:11 +0200

    dodanie szkolnego JS'a

---
+++ szkolny-js/projekt/projekt_main/script.js
@@ -0,0 +1,50 @@
+function rebelion1() {
+	document.getElementById("rebelion1").src =
+		"zdjecia/rebelion/final_dose_hoodie_przod.webp";
+}
+
+function rebelion2() {
+	document.getElementById("rebelion1").src =
+		"zdjecia/rebelion/final_dose_hoodie_tyl.webp";
+}
+
+function rebelion3() {
+	document.getElementById("rebelion2").src =
+		"zdjecia/rebelion/the-final-dose-t-shirt-przod.webp";
+}
+
+function rebelion4() {
+	document.getElementById("rebelion2").src =
+		"zdjecia/rebelion/the-final-dose-t-shirt-tyl.webp";
+}
+
+function szp1() {
+	document.getElementById("szp1").src =
+		"zdjecia/sub zero procjekt/sub-zero-project-robot-chrome-t-shirt-tyl.webp";
+}
+function szp2() {
+	document.getElementById("szp1").src =
+		"zdjecia/sub zero procjekt/sub-zero-project-robot-chrome-t-shirt-przod.webp";
+}
+
+function szp3() {
+	document.getElementById("szp2").src =
+		"zdjecia/sub zero procjekt/szp_bluza_tyl.webp";
+}
+function szp4() {
+	document.getElementById("szp2").src =
+		"zdjecia/sub zero procjekt/szp_bluza_przod.webp";
+}
+function jh1() {
+	document.getElementById("jh1").src = "zdjecia/jh/jh_przod.webp";
+}
+function jh2() {
+	document.getElementById("jh1").src = "zdjecia/jh/jh_tyl.webp";
+}
+function jn1() {
+	document.getElementById("jn1").src = "zdjecia/jh/jn_przod.webp";
+}
+function jn2() {
+	document.getElementById("jn1").src = "zdjecia/jh/jn_tyl.webp";
+}
+// the-final-dose-t-shirt-tyl.webp
